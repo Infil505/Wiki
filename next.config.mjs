b4a2a6ad0217357367c828cdb80973f80956ad58
Darwin Javier Silva Gadea <115656897+Infil505@users.noreply.github.com>
2024-10-14 00:05:48 -0600

    reparacion en nex

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     images: {
       unoptimized: true,
     },
-    basePath: '/SaveFood', // Reemplaza "nombre-proyecto" por el nombre de tu repositorio
   };
   export default nextConfig;
   
